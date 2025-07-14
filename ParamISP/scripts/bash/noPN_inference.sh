03bde1805dd12d2a8ff7f0be22d732a9fe291a03
tom1484 <tomchen2003611@gmail.com>
2025-07-14 14:09:16 -0700

    add bash scripts for full procedure of inference

---
+++ ParamISP/scripts/bash/noPN_inference.sh
@@ -0,0 +1,23 @@
+DATASET=$1
+CKPT_PATH=$2
+OUTPUT_DIR=$3
+
+python scripts/inference.py \
+--ckpt-path $CKPT_PATH \
+--dataset $DATASET \
+--group-camera-name \
+--output-dir $OUTPUT_DIR \
+--block-size 4 \
+--no-param-net
+
+python scripts/gather_inference_results.py \
+--input-dir $OUTPUT_DIR \
+--output-dir $OUTPUT_DIR/all
+
+python scripts/save_parameters.py \
+--dataset $DATASET \
+--group-camera-name \
+--output-dir $OUTPUT_DIR/all
+
+python scripts/metrics.py \
+--input-dir $OUTPUT_DIR/all
