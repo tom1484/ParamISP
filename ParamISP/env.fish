71ac38dcf02a469cb0b9f82c71d0013236be615d
tom1484 <tomchen2003611@gmail.com>
2025-07-14 14:07:57 -0700

    add env var source file

---
+++ ParamISP/env.fish
@@ -0,0 +1,5 @@
+set -x CUDA_VISIBLE_DEVICES $argv[1]
+set -x REALBLURSRC_PATCHSET_DIR patchsets/realblursrc
+set -x RAISE_PATCHSET_DIR patchsets/RAISE
+set -x S7ISP_PATCHSET_DIR patchsets/S7-ISP
+set -x FIVEK_PATCHSET_DIR patchsets/fivek
